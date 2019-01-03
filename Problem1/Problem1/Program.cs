d081cad366abd49e9423802746c403d4d81d701f
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-03 21:36:05 +0100

    Problem 1 solved

---
+++ Problem1/Problem1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Enumerable.Range(1, 999).Where(i => i % 3 == 0 || i % 5 == 0).Sum());
+        }
+    }
+}
