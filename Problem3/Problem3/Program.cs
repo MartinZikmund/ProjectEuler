ed5ad10a8a2954369c1c71ff854dd928be6dcb10
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-03 21:49:30 +0100

    Problem 3 solved

---
+++ Problem3/Problem3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = 600851475143;
+            var root = (long)Math.Sqrt(num) + 1;
+            var maxDivisor = 0L;
+            for (int i = 2; i <= root && i < num; i++)
+            {
+                while (num % i == 0)
+                {
+                    num /= i;
+                    maxDivisor = i;
+                }
+            }
+
+            if (num != 1)
+            {
+                maxDivisor = num;
+            }
+            Console.WriteLine(maxDivisor);
+        }
+    }
+}
