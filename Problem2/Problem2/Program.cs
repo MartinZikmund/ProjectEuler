68c990892a3d27f69e0a959b256c647419f6090f
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-03 21:41:59 +0100

    Problem 2 solved

---
+++ Problem2/Problem2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lastFib = 1;
+            var prelastFib = 1;
+            int sum = 0;
+            do
+            {
+                if (lastFib % 2 == 0)
+                {
+                    sum += lastFib;
+                }
+                var newFib = lastFib + prelastFib;
+                prelastFib = lastFib;
+                lastFib = newFib;                
+            } while (lastFib < 4000000);
+            Console.WriteLine(sum);
+        }
+    }
+}
