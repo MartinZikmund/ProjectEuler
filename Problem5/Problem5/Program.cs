c41b8097d5803fc986c9ef74bbc2bc082e5bba2d
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-04 13:36:23 +0100

    Solved problem 5

---
+++ Problem5/Problem5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Problem5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentMcd = 1;
+            for (int i = 2; i <= 20; i++)
+            {
+                var newMcd = currentMcd;
+                while (newMcd % i != 0)
+                {
+                    newMcd += currentMcd;
+                }
+                currentMcd = newMcd;
+            }
+
+            Console.WriteLine(currentMcd);
+        }
+    }
+}
