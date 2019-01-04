de2263f4809ec5e27584f9ebb41abd52738e884a
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-04 06:44:12 +0100

    Problem 4 solved

---
+++ Problem4/Problem4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace Problem4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((from a in Enumerable.Range(100, 999 - 100 + 1)
+             from b in Enumerable.Range(100, 999 - 100 + 1)
+             select a * b).Where(product => product.ToString().Reverse().SequenceEqual(product.ToString()))
+                .Max());
+        }
+    }
+}
