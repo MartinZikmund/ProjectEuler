3d897b5e4685a325eb108e84dd26a97b059f099a
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-06 14:15:39 +0100

    Solved problem 6

---
+++ Problem6/Problem6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Problem6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hundred = Enumerable.Range(1, 100);
+            var sumSquares = hundred.Sum(n => n * n);
+            var sum = hundred.Sum();
+            var squareSum = sum * sum;
+            Console.WriteLine(squareSum - sumSquares);
+        }
+    }
+}
