c5fc886b81d262a5d4b974b6ffa3e866a74f164d
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-07 10:00:21 +0100

    Solved problem 7

---
+++ Problem7/Problem7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem7
+{
+    class Program
+    {
+        private const int TargetPrime = 10001;
+
+        static void Main(string[] args)
+        {
+            var primes = new List<int>(TargetPrime)
+            {
+                2
+            };
+            int currentNumber = 3;
+            while (primes.Count < TargetPrime)
+            {
+                var sqrt = (int)Math.Sqrt(currentNumber) + 1;
+                bool isPrime = true;
+                for (int i = 0; i < primes.Count && primes[i] <= sqrt; i++)
+                {
+                    if (currentNumber % primes[i] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    primes.Add(currentNumber);
+                }
+                currentNumber += 2; //skipping even numbers
+            }
+            Console.WriteLine(primes.Last());
+        }
+    }
+}
