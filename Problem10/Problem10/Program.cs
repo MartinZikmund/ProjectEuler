2b691a2f08ae4389b9560340cc2819819d4325e1
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-08 15:35:24 +0100

    Solved problem 10

---
+++ Problem10/Problem10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Problem10
+{
+    class Program
+    {
+        private const int MaxNum = 2000000;
+        static void Main(string[] args)
+        {
+            var primeSum = 0UL;
+            bool[] isPrime = new bool[MaxNum];
+            Array.Fill(isPrime, true);
+            isPrime[0] = false;
+            isPrime[1] = false;
+            for (uint i = 2; i < isPrime.Length; i++)
+            {
+                if (isPrime[i])
+                {
+                    primeSum += i;
+                    var multiple = i + i;
+                    while (multiple < isPrime.Length)
+                    {
+                        isPrime[multiple] = false;
+                        multiple += i;
+                    }
+                }
+            }
+
+            Console.WriteLine(primeSum);
+        }
+    }
+}
