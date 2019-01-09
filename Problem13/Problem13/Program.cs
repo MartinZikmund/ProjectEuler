603a282a8ae5f3b9461ba79d90afa1d4b83b0306
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-09 17:20:04 +0100

    Solved problem 13

---
+++ Problem13/Problem13/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Problem13
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var input = await ReadInputAsync("Input.txt");
+            var lines = input.Split(Environment.NewLine);
+            BigInteger sum = 0;
+            foreach (var line in lines)
+            {
+                var number = BigInteger.Parse(line.Trim());
+                sum += number;
+            }
+
+            Console.WriteLine(sum.ToString().Substring(0, 10));
+        }
+
+        public static async Task<string> ReadInputAsync(string file)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using (var stream = assembly.GetManifestResourceStream(typeof(Program).Namespace + "." + file))
+            {
+                using (var streamReader = new StreamReader(stream))
+                {
+                    return await streamReader.ReadToEndAsync();
+                }
+            }
+        }
+    }
+}
