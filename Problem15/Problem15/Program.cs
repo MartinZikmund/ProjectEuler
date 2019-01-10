5480e6f2d7adaf65add0cb37b4ba038d871b912e
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-10 16:47:23 +0100

    Solved problem 15

---
+++ Problem15/Problem15/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Problem15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long solution = 1;
+            for (int i = 1; i <= 20; i++)
+            {
+                solution *= (20 + i);
+                solution /= i;
+            }
+            Console.WriteLine(solution);
+        }        
+    }
+}
