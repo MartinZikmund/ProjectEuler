4c63baed66783cf2754a1f2d404d4e2fb6818067
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-11 21:05:11 +0100

    Solved problem 16

---
+++ Problem16/Problem16/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Problem16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((((BigInteger)1) << 1000).ToString().Select(c => (c - '0')).Sum());
+        }
+    }
+}
