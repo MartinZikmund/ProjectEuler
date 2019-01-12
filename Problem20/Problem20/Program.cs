0cada6ccc43f84bb8d0bebdb97f789b06c595a21
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-12 07:03:47 +0100

    Solved problem 20

---
+++ Problem20/Problem20/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Problem20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Enumerable.Range(1, 100).Aggregate((BigInteger)1, (bigInt, n) => bigInt * n).ToString()
+                .Sum(c => c - '0'));
+        }
+    }
+}
