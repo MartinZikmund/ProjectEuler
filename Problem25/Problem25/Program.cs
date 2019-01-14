332938456b30941aeb92cfd051a74814a2830464
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-14 18:38:25 +0100

    Solved problem 25

---
+++ Problem25/Problem25/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+namespace Problem25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var termIndex = 2;
+            BigInteger previousNumber = 1, currentNumber = 1;
+            while (currentNumber.ToString().Length < 1000)
+            {
+                var nextNumber = currentNumber + previousNumber;
+                previousNumber = currentNumber;
+                currentNumber = nextNumber;
+                termIndex++;
+            }
+
+            Console.WriteLine(termIndex);
+        }
+    }
+}
