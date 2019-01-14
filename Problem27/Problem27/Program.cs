37acdf7255d514afc5a034d9966d0dd584c718e0
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-14 18:38:48 +0100

    Solved problem 27

---
+++ Problem27/Problem27/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bestN = 0;
+            var bestProduct = 0;
+            for (int a = -999; a <= 999; a++)
+            {
+                for (int b = -1000; b <= 1000; b++)
+                {
+                    //avoid calculation if can't improve
+                    if (!IsPrime(bestN * bestN + a * bestN + b)) continue;
+                    int n = 0;
+                    while (IsPrime(n * n + a * n + b))
+                    {
+                        n++;
+                    }
+                    bestN = Math.Max(n, bestN);
+                    if (bestN == n)
+                    {
+                        bestProduct = a * b;
+                    }
+                }
+            }
+            Console.WriteLine(bestProduct);
+        }
+
+        private static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            var sqrt = (int)Math.Sqrt(number);
+            var isPrime = Enumerable.Range(2, sqrt).All(d => number % d != 0);
+            return isPrime;
+        }
+    }
+}
