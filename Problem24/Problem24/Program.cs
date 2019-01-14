15c0a1761fa25c0bca03b5659ba395150bbf10f0
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-14 05:57:19 +0100

    Solved problem 24

---
+++ Problem24/Problem24/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var digits = Enumerable.Range(0, 10).ToList();
+            string result = "";
+            var order = 1000000 - 1;
+            for (int position = 9; position >= 0; position--)
+            {
+                var optionCount = Factorial(position);
+                var digitIndex = order / optionCount;
+                result += digits[digitIndex];
+                order %= optionCount;
+                digits.RemoveAt(digitIndex);
+            }
+            Console.WriteLine(result);
+        }
+
+        private static int Factorial(int number)
+        {
+            var fact = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                fact *= i;
+            }
+            return fact;
+        }
+    }
+}
