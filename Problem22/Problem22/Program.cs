5ddacad45d079260ded8b10223d76613c39fa6dc
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-14 05:49:11 +0100

    Solved problem 22 & 23

---
+++ Problem22/Problem22/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Problem22
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var sum = 0UL;
+            var namesText = await ReadEmbeddedFile("names.txt");
+            var names = namesText.Split(',').Select(name => name.Substring(1, name.Length - 2)).OrderBy(name => name).ToArray();
+            for (int i = 0; i < names.Length; i++)
+            {
+                sum += (uint)((i + 1) * names[i].Sum(c => c - 'A' + 1));
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        private static async Task<string> ReadEmbeddedFile(string fileName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using (var stream = assembly.GetManifestResourceStream(typeof(Program), fileName))
+            {
+                using (var streamReader = new StreamReader(stream))
+                {
+                    return await streamReader.ReadToEndAsync();
+                }
+            }
+        }
+    }
+}
---
+++ Problem23/Problem23/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem23
+{
+    class Program
+    {
+        private static HashSet<int> _allNumbers = new HashSet<int>(Enumerable.Range(1,28123));
+
+        static void Main(string[] args)
+        {
+            var abundantNumbers = new List<int>();
+            for (int i = 1; i <= 28123; i++)
+            {
+                var divisorSum = GetSumOfProperDivisors(i);
+                if (divisorSum > i)
+                {
+                    abundantNumbers.Add(i);
+                }
+            }
+
+            foreach (var firstNumber in abundantNumbers)
+            {
+                foreach (var secondNumber in abundantNumbers)
+                {
+                    _allNumbers.Remove(firstNumber + secondNumber);
+                }
+            }
+
+            Console.WriteLine(_allNumbers.Sum());
+        }
+
+        public static int GetSumOfProperDivisors(int number)
+        {
+            var sum = 0;
+            var sqrt = (int)Math.Sqrt(number);
+            for (int i = 1; i <= sqrt; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum += i;
+                    var otherDivisor = number / i;
+                    if (otherDivisor != i && otherDivisor != number)
+                    {
+                        sum += otherDivisor;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
