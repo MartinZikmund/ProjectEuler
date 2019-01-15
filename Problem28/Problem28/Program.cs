fe92d7f9ace2058907124e1cb1732f9c84503264
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-15 12:25:23 +0100

    Solved problem 28

---
+++ Problem28/Problem28/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Problem28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var totalSum = 0L;
+            for (int i = 1; i <= 1001; i+=2)
+            {
+                totalSum += GetCornerSum(i);
+            }
+
+            Console.WriteLine(totalSum);
+        }
+
+        public static int GetCornerSum(int width)
+        {
+            if (width == 1) return 1;
+
+            var topRight = width * width;
+
+            var topLeft = (width - 1) * width + 1;
+
+            var bottomLeft = (width - 2) * width + 2;
+
+            var bottomRight = (width - 3) * width + 3;
+
+            return topRight + topLeft + bottomLeft + bottomRight;
+        }
+    }
+}
