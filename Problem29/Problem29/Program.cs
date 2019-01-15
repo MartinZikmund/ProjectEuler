366733b73fabf9e8f07c5aad189e9f5773a5da50
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-15 16:40:05 +0100

    Brute force solved problem 29

---
+++ Problem29/Problem29/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks.Sources;
+
+namespace Problem29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BruteForce(100));
+        }
+
+        private static int BruteForce(int limit)
+        {
+            List<BigInteger> allNumbers = new List<BigInteger>();
+            for (int a = 2; a <= limit; a++)
+            {
+                for (int b = 2; b <= limit; b++)
+                {
+                    BigInteger num = new BigInteger(1);
+                    for (int bi = 0; bi < b; bi++)
+                    {
+                        num *= a;
+                    }
+
+                    allNumbers.Add(num);
+                }
+            }
+
+            return allNumbers.Distinct().Count();
+        }        
+    }
+}
