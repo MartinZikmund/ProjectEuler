38835f759e12e7ceb04822e8cae8b5029575b15c
Martin Zikmund <martinzikmund@sphereline.com>
2019-01-31 15:59:37 +0100

    Solved problem 44

---
+++ Problem44/Problem44/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem44
+{
+    class Program
+    {
+        private const int MaxN = 10000;
+
+        private static readonly HashSet<int> PentagonalNumbersHash = new HashSet<int>();
+
+        static void Main(string[] args)
+        {
+            GeneratePentagonal();
+            var orderedPentagonals = PentagonalNumbersHash.OrderBy(n => n).ToArray();
+            foreach (var difference in orderedPentagonals)
+            {
+                foreach (var firstNum in orderedPentagonals)
+                {
+                    var result = firstNum + difference;
+                    if (PentagonalNumbersHash.Contains(result) &&
+                        PentagonalNumbersHash.Contains(firstNum + result))
+                    {
+                        Console.WriteLine(difference);
+                        return;
+                    }
+                }
+            }
+        }
+
+        private static void GeneratePentagonal()
+        {
+            for (int n = 1; n <= MaxN; n++)
+            {
+                var number = n * (3 * n - 1) / 2;
+                PentagonalNumbersHash.Add(number);
+            }
+        }
+    }
+}
