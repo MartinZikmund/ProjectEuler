702b1616059466e5d0efa2c4d49d177c21ca7612
Martin Zikmund <martinzikmund@live.com>
2019-02-17 09:15:34 +0100

    Solved problem 58

---
+++ Problem58/Problem58/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Problem58
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeCount = 0;
+            int currentLength = 1;
+            do
+            {
+                currentLength += 2;
+                var bottomRight = currentLength * currentLength;
+                var bottomLeft = bottomRight - currentLength + 1;
+                var topLeft = bottomLeft - currentLength + 1;
+                var topRight = topLeft - currentLength + 1;
+                if (IsPrime(bottomRight)) primeCount++;
+                if (IsPrime(bottomLeft)) primeCount++;
+                if (IsPrime(topLeft)) primeCount++;
+                if (IsPrime(topRight)) primeCount++;
+            } while ((currentLength / 2) * 4 + 1 <= 10 * primeCount);
+            Console.WriteLine(currentLength);
+        }
+
+        private static bool IsPrime(int number)
+        {
+            var sqrt = (int)Math.Sqrt(number);
+            for (int i = 2; i <= sqrt; i++)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
